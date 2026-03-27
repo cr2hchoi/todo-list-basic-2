c96b8b26bcfc9c0a337878aa878a88627ba7d9fd
cr2hChoi <20231046@sungshin.ac.kr>
2026-03-27 17:50:32 +0900

    배포 설정 추가

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,8 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+  // base 부분을 본인의 GitHub 저장소(Repository) 이름으로 적어주세요!
+  // 앞뒤에 슬래시(/) 넣는 거 잊지 마시고요!
+  base: '/todo-list-basic-2/',
   plugins: [vue()],
-});
+})
